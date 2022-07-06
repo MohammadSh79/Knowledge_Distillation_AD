95752c12156dd9831bb6e18dedfcaa6aad5a4a89
MohammadSh79 <MohammadShams956@gmail.com>
2022-07-06 14:39:08 +0430

    a

--- test_functions.py
+++ test_functions.py
@@ -361,7 +361,8 @@ def calculate_scores(model, test_dataloader):
         prediction = model.forward(X.cuda())
         y_pred.append(prediction)
 
-    # y_pred = np.array(y_pred)
+    print(y_true.shape)
+    print(y_pred.shape)
 
     precision, recall, f1, support = precision_recall_fscore_support(y_true, y_pred, zero_division=1)
     confusionMatrix = confusion_matrix(y_true, y_pred)
