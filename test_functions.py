6f2b9e2fd23fdf81f634f778558a248c65c83435
MohammadSh79 <MohammadShams956@gmail.com>
2022-07-06 19:04:00 +0430

    a

--- test_functions.py
+++ test_functions.py
@@ -359,7 +359,7 @@ def calculate_scores(model, test_dataloader):
     for (X, Y) in test_dataloader:
         y_true = np.append(y_true, Y, axis=0)
         prediction = model.forward(X.cuda())
-        prediction.dtype
+        np.array(prediction)
         y_pred = np.append(y_pred, prediction, axis=0)
 
     print(y_true.shape)
