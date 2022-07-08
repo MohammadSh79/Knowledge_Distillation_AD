212620d9d0e746aafa9428d2dcb991559d35e39e
MohammadSh79 <MohammadShams956@gmail.com>
2022-07-08 15:02:46 +0430

    a

--- train.py
+++ train.py
@@ -135,10 +135,10 @@ def train2(config):
             if X.shape[1] == 1:
                 X = X.repeat(1, 3, 1, 1)
             X = Variable(X).cuda()
-            Y = Variable(Y).cuda().view((1, 64))
+            Y = Variable(Y).cuda().view((64, 1))
 
             output_pred = model.forward(X)
-            output_pred = output_pred[-1].reshape((1, 64))
+            output_pred = output_pred[-1].reshape((64, 1))
 
             print(Y.size())
             print(output_pred.size())
