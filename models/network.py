96981599747c530ee738dab73e3194a426051412
MohammadSh79 <MohammadShams956@gmail.com>
2022-07-08 14:03:55 +0430

    a

--- models/network.py
+++ models/network.py
@@ -59,7 +59,7 @@ def make_layers(cfg, use_bias, batch_norm=False):
                 layers += [conv2d, nn.ReLU(inplace=True)]
             in_channels = cfg[i]
     layers += [nn.Flatten()]
-    layers += [nn.Linear(in_features=4096, out_features=10)]
+    layers += [nn.Linear(in_features=1024, out_features=10)]
     layers += [nn.Linear(in_features=10, out_features=1)]
     return nn.Sequential(*layers)
 
