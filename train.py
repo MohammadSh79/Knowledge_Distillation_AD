6b70d95c2931fe2bd57a662abe05459986db20ea
MohammadSh79 <MohammadShams956@gmail.com>
2022-07-08 17:51:35 +0430

    Added train2 function

--- train.py
+++ train.py
@@ -88,12 +88,57 @@ def train(config):
         #               'wb') as f:
         #         pickle.dump(roc_aucs, f)
 
+def train2(config):
+    print("============================ [Training according to Y] ============================")
+    normal_class = config["normal_class"]
+    learning_rate = float(config['learning_rate'])
+    num_epochs = config["num_epochs"]
+
+    train_dataloader, test_dataloader = load_data(config)
+    vgg, model = get_networks(config)
+
+    criterion = torch.nn.MSELoss()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    losses = []
+    roc_aucs = []
+
+    for epoch in range(6):
+        model.train()
+        epoch_loss = 0
+        for data in train_dataloader:
+            X = data[0]
+            Y = data[1]
+            if X.shape[1] == 1:
+                X = X.repeat(1, 3, 1, 1)
+            X = Variable(X).cuda()
+            Y = Variable(Y).cuda().view((-1, 1)).to(torch.float32)
+
+            output_pred = model.forward(X)
+            output_pred = output_pred[-1].reshape((-1, 1))
+
+
+            total_loss = criterion(output_pred, Y)
+
+            # Add loss to the list
+            epoch_loss += total_loss.item()
+            losses.append(total_loss.item())
+
+            # Clear the previous gradients
+            optimizer.zero_grad()
+            # Compute gradients
+            total_loss.backward()
+            # Adjust weights
+            optimizer.step()
+
+        print('epoch [{}/{}], loss:{:.4f}'.format(epoch + 1, 5, epoch_loss))
 
 def main():
     args = parser.parse_args()
     config = get_config(args.config)
     train(config)
-
+    train2(config)
 
 if __name__ == '__main__':
     main()
